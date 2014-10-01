2ac09aac862e857a9e9e48708544202eaf3a3e2d
Xintong Shi (Summer) <Summer@10.0.1.8>
2014-10-01 01:21:31 -0700

    Picture taken feature added(still working on better layout)

--- src/com/comp490/studybuddy/textnote/TextNote.java
+++ src/com/comp490/studybuddy/textnote/TextNote.java
@@ -1,17 +1,29 @@
 package com.comp490.studybuddy.textnote;
 
-import com.example.studybuddy.R;
-import com.example.studybuddy.R.id;
-import com.example.studybuddy.R.layout;
-import com.example.studybuddy.R.menu;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.studybuddy.R;
 
 public class TextNote extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,4 +48,64 @@ public class TextNote extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	public static final int MEDIA_TYPE_VIDEO = 2;
+	
+	private ImageView a = null;
+	@SuppressWarnings("unused")
+	private Uri fileUri;
+	private static File mediaFile;
+
+	public void takePhoto(View view) {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+		//intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		startActivityForResult(intent, 1);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		a = (ImageView) findViewById(R.id.imageView1);
+		if (resultCode == Activity.RESULT_OK) {
+				Bundle extras = data.getExtras();
+				Bitmap btm = (Bitmap) extras.get("data");
+				FileOutputStream b;
+				try {
+					b = new FileOutputStream(mediaFile);
+					btm.compress(Bitmap.CompressFormat.JPEG, 100, b);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+				a.setImageBitmap(btm);
+		}
+	}
+	
+	private static Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	@SuppressLint("SimpleDateFormat")
+	private static File getOutputMediaFile(int type) {
+		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MyCameraApp");
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d("MyCameraApp", "failed to create directory");
+				return null;
+			}
+		}
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+		} 
+		else if (type == MEDIA_TYPE_VIDEO) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
+		} 
+		else {
+			return null;
+		}
+		return mediaFile;
+	}
 }
+	
