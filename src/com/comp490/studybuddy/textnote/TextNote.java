b9083416cac702e7395b2be2f3c81a6d98358784
Anthony Sager <snigglyfoo@gmail.com>
2014-10-04 19:12:04 -0700

    Records sound, plays back. Note: emulator cannot record.

--- src/com/comp490/studybuddy/textnote/TextNote.java
+++ src/com/comp490/studybuddy/textnote/TextNote.java
@@ -1,12 +1,23 @@
+/*
+Author Name and CSUN ID: Anthony Summer Nik
+Date started: 9/26/2014
+Date Completed:
+Peer review: 
+Date:
+Team members:
+Contributing team members: Anthony Summer Nik
+ */
+
 package com.comp490.studybuddy.textnote;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -30,11 +41,11 @@ import com.comp490.studybuddy.R;
 
 public class TextNote extends Activity {
 	
-	boolean micExists;
-	MediaRecorder recorder;
-	MediaPlayer player;
-	int count = 1;
+	boolean micExists, recording, playing = false;
+	MediaRecorder recorder = null;
+	MediaPlayer player = null;
 	private static final String LOG_TAG = "Sound Record";
+	String filename;
 
 	
 	public static final int MEDIA_TYPE_IMAGE = 1;
@@ -58,37 +69,44 @@ public class TextNote extends Activity {
 		getMenuInflater().inflate(R.menu.text_note, menu);
 		View v = (View) menu.findItem(R.id.action_record_sound).getActionView();
 		
-		//listeners for Record action view menu
-		final Button b = (Button) v.findViewById(R.id.bActionSoundRecord);
-		b.setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v1) {
-				if (hasMic()){
-					b.setTextColor(Color.RED);
-					clickie();
-					startRecording();
+		//listeners for Record action view menu		
+		if (hasMic()){
+			final Button b = (Button) v.findViewById(R.id.bActionSoundRecord);
+			b.setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v1) {
+					if (!recording){
+						b.setTextColor(Color.RED);					
+						startRecording();
+					}
 				}
-			}
-		});
-		v.findViewById(R.id.ibActionSoundPlay).setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v1) {
-				clickie();
-			}
-		});
-		v.findViewById(R.id.ibActionSoundStop).setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v1) {
-				b.setTextColor(Color.WHITE);
-			}
-		});
-		v.findViewById(R.id.ibActionSoundPause).setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v1) {
-				clickie();
-			}
-		});
-		v.findViewById(R.id.ibActionSoundSave).setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v1) {
-				clickie();
-			}
-		});		
+			});
+			v.findViewById(R.id.ibActionSoundPlay).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v1) {
+					if (!playing || !recording){
+						playSound();
+					}
+				}
+			});
+			v.findViewById(R.id.ibActionSoundStop).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v1) {
+					if (playing || recording){
+						b.setTextColor(Color.WHITE);
+						stopRecordOrPlay();
+					}
+				}
+			});
+			v.findViewById(R.id.ibActionSoundPause).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v1) {
+					clickie();
+				}
+			});
+			v.findViewById(R.id.ibActionSoundSave).setOnClickListener(new View.OnClickListener(){
+				public void onClick(View v1) {
+					clickie();
+				}
+			});	
+		}
+	
 		return true;
 	}
 	
@@ -141,7 +159,6 @@ public class TextNote extends Activity {
 		return Uri.fromFile(getOutputMediaFile(type));
 	}
 
-	@SuppressLint("SimpleDateFormat")
 	private static File getOutputMediaFile(int type) {
 		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MyCameraApp");
 		if (!mediaStorageDir.exists()) {
@@ -150,7 +167,7 @@ public class TextNote extends Activity {
 				return null;
 			}
 		}
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
 		if (type == MEDIA_TYPE_IMAGE) {
 			mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
 		} 
@@ -168,21 +185,81 @@ public class TextNote extends Activity {
 		return pm.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
 	}
 	
-	private void startRecording() {
-		try {
+	private void startRecording() { //start actionView button press
+
+			if (playing && player != null){
+				stopPlayback();
+			}
+			
+			//TO DO: option: record from end of previous recording
 			Toast.makeText(this, "Recording", Toast.LENGTH_SHORT).show();
 			recorder = new MediaRecorder();
-			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-			recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-			String filename = Environment.getExternalStorageDirectory()
-					.getAbsolutePath() + "/Sound" + count;
-			count++;
-			//filename += new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-			recorder.setOutputFile(filename);
-		} catch (IllegalStateException e) {
-			Log.e(LOG_TAG, "startRecording() broke");
+			try {
+				recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+				recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+				filename = Environment.getExternalStorageDirectory()
+						.getAbsolutePath() + "/SB_Audio_" + new SimpleDateFormat("MM-dd-yyyy_KK-mm-ss-a", Locale.getDefault()).format(new Date())
+						+ ".3gp";
+				recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+				recorder.setOutputFile(filename);
+
+				recorder.prepare();
+				recorder.start();
+				recording = true;
+			} catch (IllegalStateException e) {
+				Log.e(LOG_TAG, "startRecording() broke illegalstate");
+				e.printStackTrace();
+			} catch (IOException e) {
+				Log.e(LOG_TAG, "startRecording() broke ioexception");
+				e.printStackTrace();
+			}
+	}
+	
+	private void stopRecordOrPlay(){ //stop actionView button press
+		if (playing){
+			stopPlayback();
+		}
+		if (recording){
+			stopRecording();
+		}
+	}
+	
+	private void stopPlayback(){
+		Toast.makeText(this, "Stopped Playback", Toast.LENGTH_SHORT).show();
+		player.stop();
+		player.release();
+		player = null;
+	}
+	
+	private void stopRecording(){
+		Toast.makeText(this, "Stopped Recording", Toast.LENGTH_SHORT).show();
+		recorder.stop();
+		recorder.reset();
+		recorder.release();
+		recorder = null;
+	}
+	
+	private void playSound(){
+		try {
+			player = new MediaPlayer();
+			player.setDataSource(filename);
+			player.prepare();
+			player.start();
+			playing = true;
+		} catch (Exception e) {
+			Log.e(LOG_TAG, "playBack() broke");
 		}
 	}
 }
+
+/* SOUND: TO DO 10/4/2014 
+ * Allow play/record to continue while activity paused/stopped
+ * Remove errors (they have no effect)
+ * Sound file selection
+ * Create buttons representing saved sound files, allow them to display actionView controls. 
+ * Save the sound files and buttons for later use
+ * Allow the user to rename the sound files
+ * Put sound files in a directory
+ * */
+
 	
