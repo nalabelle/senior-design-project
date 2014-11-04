e800a9bcd5dd9e3441252343cb46ef2402976aa0
nalabelle <nalabelle@gmail.com>
2014-11-04 14:09:47 -0800

    massive refactoring on calendar moved library to new location

--- src/com/comp490/studybuddy/calendar/CalenActivity.java
+++ src/com/comp490/studybuddy/calendar/CalenActivity.java
@@ -19,20 +19,18 @@
 
 package com.comp490.studybuddy.calendar;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -40,7 +38,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
@@ -48,12 +45,11 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.comp490.studybuddy.R;
-import com.comp490.studybuddy.textnote.TextNote;
 
 public class CalenActivity extends Activity {
 	
-	private Calendar calendar;
-    private int month, year;
+	private DateTime calendar;
+	private DateTime originalDate;
     private TextView currentMonth;
     
     //Days of week, add events. TODO
@@ -65,8 +61,7 @@ public class CalenActivity extends Activity {
     private GridView calendarGrid;
     private GridCellAdapter adapter;
     
-    private final DateFormat dateFormatter = new DateFormat();
-    private static final String dateTemplate = "MMMM yyyy";
+    private final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("MMMM yyyy");
 
     private int width;
     private static int height;
@@ -81,13 +76,11 @@ public class CalenActivity extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         width = metrics.widthPixels;
         height = metrics.heightPixels;
-		
-		calendar = Calendar.getInstance(Locale.getDefault());
-		month = calendar.get(Calendar.MONTH) + 1;
-        year = calendar.get(Calendar.YEAR);
 
+        this.calendar = new DateTime();
+        this.originalDate = new DateTime();
         currentMonth = (TextView) this.findViewById(R.id.curr_month);
-        currentMonth.setText(dateFormatter.format(dateTemplate, calendar.getTime()));
+        currentMonth.setText(calendar.toString(dateFormatter));
         
         prevButton = (ImageButton) this.findViewById(R.id.prevMonth);
         nextButton = (ImageButton) this.findViewById(R.id.nextMonth);
@@ -99,50 +92,18 @@ public class CalenActivity extends Activity {
         calendarGrid = (GridView) this.findViewById(R.id.gridView1);
         
         // Grid --> Calendar
-        adapter = new GridCellAdapter(getApplicationContext(), R.id.grid_day, month, year);
+        adapter = new GridCellAdapter(getApplicationContext(), R.id.grid_day);
         adapter.notifyDataSetChanged();
         calendarGrid.setAdapter(adapter);
 	}
 	
-    private void changeCalendarDisplay(int month, int year)
+    private void changeCalendarDisplay()
     {
-        adapter = new GridCellAdapter(getApplicationContext(), R.id.grid_day, month, year);
-        calendar.set(year, month - 1, calendar.get(Calendar.DAY_OF_MONTH));
-        currentMonth.setText(dateFormatter.format(dateTemplate, calendar.getTime()));
+        adapter = new GridCellAdapter(getApplicationContext(), R.id.grid_day);
+        currentMonth.setText(calendar.toString(dateFormatter));
         adapter.notifyDataSetChanged();
         calendarGrid.setAdapter(adapter);
     }
-	
-	public void movePast(View v)
-	{
-		if (month <= 1)
-        {
-            month = 12;
-            year--;
-        }
-		else
-        {
-            month--;
-        }
-    
-		changeCalendarDisplay(month, year);
-	}
-	
-	public void moveFuture(View v)
-	{
-		if (month > 11)
-		{
-			month = 1;
-            year++;
-        }
-        else
-        {
-        	month++;
-        }
-		
-		changeCalendarDisplay(month, year);
-    }
-
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -163,158 +124,82 @@ public class CalenActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}//Autogen
 	
+	
+	public void movePast(View v)
+	{
+		this.calendar = this.calendar.minusMonths(1);
+		changeCalendarDisplay();
+	}
+	
+	public void moveFuture(View v)
+	{
+		this.calendar = this.calendar.plusMonths(1);
+		changeCalendarDisplay();
+    }
+	
 	//Grid --> Calendar
 	public class GridCellAdapter extends BaseAdapter implements OnClickListener
 	{
         private final Context context;
 
         private final List<String> list;
-        private static final int DAY_OFFSET = 1;
         
-        private final String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        private final int[] daysOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        private final int month, year;
-        private int daysInMonth;
-        private int currDayOfMonth;
-        private int currWeekDay;
         private Button gridcell;
         private TextView num_events_per_day;
         private final HashMap<String, Integer> eventsPerMonthMap;
         //better way? Locale based
-        private final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+        //private final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
 
-        public GridCellAdapter(Context context, int cellID, int month, int year)
+        public GridCellAdapter(Context context, int cellID)
         {
                 super();
                 this.context = context;
                 this.list = new ArrayList<String>();
-                this.month = month;
-                this.year = year;
                 
-                Calendar calendar = Calendar.getInstance();
-                setCurrDayOfMonth(calendar.get(Calendar.DAY_OF_MONTH));
-                setCurrWeekDay(calendar.get(Calendar.DAY_OF_WEEK));
-
                 // Print Month
-                printMonth(month, year);
+                printMonth();
 
                 // Find Number of Events
-                eventsPerMonthMap = findEventsMonth(year, month);
+                eventsPerMonthMap = findEventsMonth(calendar.getYear(), calendar.getMonthOfYear());
         }
         
-        //print to cells
-        private void printMonth(int m, int y)
-        {
-            int prevMonthPadding = 0;
-            int daysInPrevMonth = 0;
-            
-            int prevMonth = 0;
-            int prevYear = 0;
-            
-            int nextMonth = 0;
-            int nextYear = 0;
-
-            int currentMonth = m - 1;
-            daysInMonth = getDaysinMonth(currentMonth);
-
-            GregorianCalendar cal = new GregorianCalendar(y, currentMonth, 1);
-
-            if (currentMonth == 11) //Dec
-            {
-                prevMonth = currentMonth - 1;
-                daysInPrevMonth = getDaysinMonth(prevMonth);
-                nextMonth = 0;
-                prevYear = y;
-                nextYear = y + 1;
-            }
-            else if (currentMonth == 0) // Jan
-            {
-                prevMonth = 11;
-                prevYear = y - 1;
-                nextYear = y;
-                daysInPrevMonth = getDaysinMonth(prevMonth);
-                nextMonth = 1;
-                 
-            }
-            else //Not dec && Not jan
-            {
-                prevMonth = currentMonth - 1;
-                nextMonth = currentMonth + 1;
-                nextYear = y;
-                prevYear = y;
-                daysInPrevMonth = getDaysinMonth(prevMonth); 
-            }
-
-           //Pad front of month
-            int currentWeekDay = cal.get(Calendar.DAY_OF_WEEK) - 1;
-            prevMonthPadding = currentWeekDay;
-
-            if (cal.isLeapYear(cal.get(Calendar.YEAR)) && m == 1)
-            {
-                ++daysInMonth;
-            }
-
-            // Previous Month days, fill in beginning of curr month if necessary
-            for (int i = 0; i < prevMonthPadding; i++)
-            {
-                list.add(String.valueOf((daysInPrevMonth - prevMonthPadding + DAY_OFFSET) + i) + "-GREY" + "-" + getMonthString(prevMonth) + "-" + prevYear);
-            }
-
-            // Current Month Days
-            for (int i = 1; i <= daysInMonth; i++)
-            {
-                if (i == getCurrDayOfMonth())
-                {
-                    list.add(String.valueOf(i) + "-BLUE" + "-" + getMonthString(currentMonth) + "-" + y);
-                }
-                else
-                {
-                    list.add(String.valueOf(i) + "-WHITE" + "-" + getMonthString(currentMonth) + "-" + y);
-                }
-            }
-
-            // Fill in end of curr months, if necessary
-            for (int i = 0; i < list.size() % 7; i++)
-            {
-                list.add(String.valueOf(i + 1) + "-GREY" + "-" + getMonthString(nextMonth) + "-" + nextYear);
-            }
-        } //End print Month
-        
-        public String getMonthString(int i)
-        {
-        	return months[i];
-        }
-
-        public int getDaysinMonth(int i)
-        {
-        	return daysOfMonth[i];
-        }
-
         public String getItem(int position)
         {
             return list.get(position);
         }
         
-        public int getCurrDayOfMonth()
-        {
-            return currDayOfMonth;
-        }
+		//Where to store/ retrieve events?
+		//String == event desc.,  Integer is the day
+		private HashMap<String, Integer> findEventsMonth(int year, int month)
+		{
+		    HashMap<String, Integer> map = new HashMap<String, Integer>();
+		    return map;
+		}
         
-        public void setCurrDayOfMonth(int currDayOfMonth)
+        //print to cells
+        private void printMonth()
         {
-            this.currDayOfMonth = currDayOfMonth;
-        }
+        	//Set up dates with padding.
+        	DateTime startDate = calendar.withDayOfMonth(calendar.dayOfMonth().getMinimumValue()).withDayOfWeek(calendar.dayOfWeek().getMinimumValue());
+        	DateTime endDate = calendar.withDayOfMonth(calendar.dayOfMonth().getMaximumValue()).withDayOfWeek(calendar.dayOfWeek().getMaximumValue());
+        	
+        	//Iterate over every date in the period.
+        	for (DateTime date = startDate; date.isBefore(endDate.plusDays(1)); date = date.plusDays(1)) {
+        		//Check if we're in the right month.
+        		if(!calendar.monthOfYear().equals(date.monthOfYear())) {
+        			if(date.isBefore(calendar.withDayOfMonth(calendar.dayOfMonth().getMinimumValue())))
+        				list.add(date.getDayOfMonth() + "-GREY" + "-" + date.monthOfYear().getAsText() + "-" + date.year().getAsText());
+        			else if(date.isAfter(calendar.withDayOfMonth(calendar.dayOfMonth().getMaximumValue())))
+        				list.add(date.getDayOfMonth() + "-GREY" + "-" + date.monthOfYear().getAsText() + "-" + date.year().getAsText());
+        		} else { //we're in this month.
+        			if(originalDate.withTimeAtStartOfDay().isEqual(date.withTimeAtStartOfDay())) //today
+	        			list.add(date.getDayOfMonth() + "-WHITE" + "-" + date.monthOfYear().getAsText() + "-" + date.year().getAsText());
+        			else
+        				list.add(date.getDayOfMonth() + "-BLUE" + "-" + date.monthOfYear().getAsText() + "-" + date.year().getAsText());
+        		}
+        	}
+        } //End print Month
         
-        public int getCurrWeekDay()
-        {
-            return currWeekDay;
-        }
-        
-        public void setCurrWeekDay(int currWeekDay)
-        {
-            this.currWeekDay = currWeekDay;
-        }
         
         @Override
         public int getCount()
@@ -328,14 +213,6 @@ public class CalenActivity extends Activity {
             return position;
         }
         
-        //Where to store/ retrieve events?
-        //String == event desc.,  Integer is the day
-        private HashMap<String, Integer> findEventsMonth(int year, int month)
-        {
-            HashMap<String, Integer> map = new HashMap<String, Integer>();
-            return map;
-        }
-
         @Override
         public View getView(int position, View convertView, ViewGroup parent)
             {
