74c2fc75eaeea52769b0e480d89dfc9fe381c1df
Alex Swanson <swansonalex@gmail.com>
2014-11-04 13:28:27 -0800

    Beginning to add an "Add Event" activity to the calendar

---
+++ src/com/comp490/studybuddy/calendar/AddEvent.java
@@ -0,0 +1,15 @@
+package com.comp490.studybuddy.calendar;
+
+import com.comp490.studybuddy.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddEvent extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		 setContentView(R.layout.add_task_calendar);
+	}
+
+}
