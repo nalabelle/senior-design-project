ea4c8411fada3fdd2cb5cad5442e38ea77b823c5
Anthony Sager <snigglyfoo@gmail.com>
2015-02-21 01:22:34 -0800

    Beautification: Changed splash to trademark free design

--- src/com/comp490/studybuddy/SplashScreen.java
+++ src/com/comp490/studybuddy/SplashScreen.java
@@ -2,19 +2,29 @@ package com.comp490.studybuddy;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
  
 public class SplashScreen extends Activity 
 {
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 1750;
+    private static int SPLASH_TIME_OUT = 3000; //1750;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);        
         setContentView(R.layout.activity_splash_screen);
+        
+        // Font Apache 2.0 License 
+        //http://www.fontsquirrel.com/license/permanent-marker 
+        Typeface font = Typeface.createFromAsset(getAssets(), "font/PermanentMarker.ttf");
+        TextView tv = (TextView) findViewById(R.id.splashTextView);
+        tv.setTypeface(font);
+        tv.startAnimation(AnimationUtils.loadAnimation(SplashScreen.this, R.animator.fade));
  
         new Handler().postDelayed(new Runnable() 
         {
